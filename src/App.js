9ae6cc3a119b9eb41397ea8729b7d6b8935da668
Bogdan <podowko@gmail.com>
2018-11-04 10:30:15 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Layout from './hoc/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+          <h1>Layout</h1>
+      </Layout>
+    );
+  }
+}
+
+export default App
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import styles from './Layout.module.scss';
+
+class Layout extends Component {
+    render() {
+        return (
+            <div className={styles.Layout}>
+                <main>
+                    { this.props.children }
+                </main>
+            </div>
+        )
+    }
+}
+
+export default Layout
